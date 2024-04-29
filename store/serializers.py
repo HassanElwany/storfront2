365915238ffe3e843873a8b7d5539ebd615af84c
Hassan Elwany <elwany_hassan@hotmail.com>
2024-04-29 11:06:28 +0300

    initial commit

---
+++ store/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from store.models import Product, Collection, Review
+from decimal import Decimal
+
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title', 'product_count']
+
+    product_count = serializers.IntegerField(read_only=True)   
+
+    
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'slug', 'inventory',
+            'unit_price', 'price_with_tax', 'collection']
+
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+    price_with_tax = serializers.SerializerMethodField(
+        method_name='calculate_tax')
+
+    def calculate_tax(self, product: Product):
+        return round((product.unit_price * Decimal(1.1)), 2)
+
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['id', 'date', 'name', 'description']
+
+    def create(self, validated_data):
+        product_id = self.context['product_id']
+        return Review.objects.create(product_id=product_id, **validated_data)
